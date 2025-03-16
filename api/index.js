e98bc89f08e43e41269069c3dd8e97b36d5ff8e6
Ishita Khichar <ishitakhichar@gmail.com>
2025-03-16 10:26:15 +0530

    update

--- api/index.js
+++ api/index.js
@@ -26,10 +26,10 @@ app.get("/", (req, res) => {
 });
 
 // Routes
-app.use('/auth', authRoutes);
-app.use('/destinations', destinationRoutes);
-app.use('/challenges', challengeRoutes);
-app.use('/users', userRoutes);
+app.use('api/auth', authRoutes);
+app.use('api/destinations', destinationRoutes);
+app.use('api/challenges', challengeRoutes);
+app.use('api/users', userRoutes);
 
 // Health check route
 app.get('/health', (req, res) => {
